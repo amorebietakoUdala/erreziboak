7c59d5ab892a932ffe38bc387a05aca438fcbff9
Amorebietako Udala <informatika@amorebieta.eus>
2020-02-10 09:00:43 +0100

    - User Administration working.

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'popper.js';
+
+import '../css/app.scss';
+
+$(document).ready(function(){
+    $('#js-locale-es').on('click',function (e) {
+		e.preventDefault();
+		var current_locale = $('html').attr("lang");
+		if ( current_locale === 'es') {
+			return;
+		}
+		var location = window.location.href;
+		var location_new = location.replace("/eu/","/es/");
+		window.location.href=location_new;
+    });
+    $('#js-locale-eu').on('click',function (e) {
+		e.preventDefault();
+		var current_locale = $('html').attr("lang");
+		if ( current_locale === 'eu') {
+			return;
+		}
+		var location = window.location.href;
+		var location_new = location.replace("/es/","/eu/");
+		window.location.href=location_new;
+    });
+	$('.js-back').on('click',function(e){
+		e.preventDefault();
+		var url = e.currentTarget.dataset.url;
+		document.location.href=url;
+	});
+});
