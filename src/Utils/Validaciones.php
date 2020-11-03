6fa503bbbead2466264f4667ff577f08c0214312
Amorebietako Udala <informatika@amorebieta.eus>
2020-11-03 13:18:39 +0100

    - Removed unused code.

--- src/Utils/Validaciones.php
+++ src/Utils/Validaciones.php
@@ -122,46 +122,4 @@ class Validaciones
 
         return $d && $d->format($format) === $date;
     }
-
-    public function validateCif($cif)
-    {
-        $cif_codes = 'JABCDEFGHI';
-
-        $sum = (string) self::getCifSum($cif);
-        $n = (10 - substr($sum, -1)) % 10;
-
-        if (preg_match('/^[ABCDEFGHJNPQRSUVW]{1}/', $cif)) {
-            if (in_array($cif[0], array('A', 'B', 'E', 'H'))) {
-                // Numerico
-                return $cif[8] == $n;
-            } elseif (in_array($cif[0], array('K', 'P', 'Q', 'S'))) {
-                // Letras
-                return $cif[8] == $cif_codes[$n];
-            } else {
-                // Alfanumérico
-                if (is_numeric($cif[8])) {
-                    return $cif[8] == $n;
-                } else {
-                    return $cif[8] == $cif_codes[$n];
-                }
-            }
-        }
-
-        return false;
-    }
-
-    public function getCifSum($cif)
-    {
-        $sum = $cif[2] + $cif[4] + $cif[6];
-
-        for ($i = 1; $i < 8; $i += 2) {
-            $tmp = (string) (2 * $cif[$i]);
-
-            $tmp = $tmp[0] + ((2 == strlen($tmp)) ? $tmp[1] : 0);
-
-            $sum += $tmp;
-        }
-
-        return $sum;
-    }
 }
