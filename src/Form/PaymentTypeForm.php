464ce4cd3d2a7e6968584280f8d4e14033a73571
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2022-09-14 14:20:18 +0200

    Fixed error with font-awesome

--- src/Form/PaymentTypeForm.php
+++ src/Form/PaymentTypeForm.php
@@ -32,6 +32,7 @@ class PaymentTypeForm extends AbstractType
             $builder->add('date_from', DateTimeType::class, [
         'widget' => 'single_text',
         'html5' => false,
+        'help' => 'YYYY/MM/DD HH:MM',
         'format' => 'yyyy/MM/dd HH:mm',
         'label' => 'payment.from',
             'disabled' => $readonly,
@@ -39,6 +40,7 @@ class PaymentTypeForm extends AbstractType
         ->add('date_to', DateTimeType::class, [
         'widget' => 'single_text',
         'html5' => false,
+        'help' => 'YYYY/MM/DD HH:MM',
         'format' => 'yyyy/MM/dd HH:mm',
         'label' => 'payment.to',
             'disabled' => $readonly,
@@ -47,7 +49,7 @@ class PaymentTypeForm extends AbstractType
             $builder->add('timestamp', DateTimeType::class, [
         'widget' => 'single_text',
         'html5' => false,
-        'format' => 'yyyy-MM-dd HH:mm:ss',
+        'format' => 'yyyy/MM/dd HH:mm',
         'attr' => ['class' => 'js-datepicker'],
         'label' => 'payment.timestamp',
             'disabled' => $readonly,
