ab17dec24511fbbdcea3b0ec17a2252155558e5a
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2022-11-03 10:36:50 +0100

    Added roles for each function

--- assets/js/user/new.js
+++ assets/js/user/new.js
@@ -3,9 +3,11 @@ import '../../css/user/new.scss';
 import $ from 'jquery';
 
 $(document).ready(function(){
-//    $('.js-save').on('click',function (e) {
-//        console.log('saveButtonClicked!');
-//        $(document.user).attr('action', $(e.currentTarget).data("url"));
-//        document.user.submit();
-//    });
+   // We do this to avoid autocompletion from the browser. We put readonly by default on html and remove it here
+   if($('#user_password_first').length > 0) {
+      $('#user_password_first').removeAttr('readonly');
+   }
+   if($('#user_password_second').length > 0) {
+      $('#user_password_second').removeAttr('readonly');
+   }
 });
