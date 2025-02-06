16b24685acd892f0ae84beb42f2a96e6cfe69b82
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2025-02-06 09:49:41 +0100

    Added default packages

--- R/convertir.sh
+++ R/convertir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 iconv -f=Windows-1252 -t=UTF-8 $1 > $1.utf8
-Rscript convertir.R $1.utf8 $2 $3
+Rscript --default-packages="stringr,dplyr,lubridate,utils" convertir.R $1.utf8 $2 $3
 echo "${1%.*}"
 zip -j ${1%.*}.zip ${1%.*}.*
 exit $?
